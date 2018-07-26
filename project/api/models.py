78ae3758d71982a0bd80628c6bb014772f955cf3
foonia <ljh9032a@naver.com>
2018-07-26 12:07:55 +0900

    Wifi model class 추가

--- project/api/models.py
+++ project/api/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Wifi(models.Model):
+    FACILITY = (
+        ('a', '관공서'),
+        ('b', '지역문화시설'),
+        ('c', '편의시설'),
+        ('d', '서민복지시설'),
+        ('e', '교통시설'),
+        ('z', '기타'),
+    )
+
+    place = models.CharField(max_length=128, help_text='설치장소명')
+    place_description = models.CharField(max_length=128, help_text='설치장소상세설명')
+    city = models.CharField(max_length=128, help_text='설치시명')
+    country = models.CharField(max_length=128, help_text='설치군구명')
+    facility = models.CharField(max_length=128, choices=FACILITY, help_text='설치시설구분')
+    service_provider = models.CharField(max_length=128, help_text='서비스제공사')
+    wifi_ssid = models.CharField(max_length=128, help_text='와이파이SSID')
+    installed_at = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
